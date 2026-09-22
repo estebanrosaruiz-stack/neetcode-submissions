e27e21aafd271d9dad7b1c2eb74656fe94dd0c51
estebanrosaruiz-stack <225858315+estebanrosaruiz-stack@users.noreply.github.com>
2026-09-22 15:49:55 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(const string& s, const string& t) {
+        if (s.size() != t.size()) return false;
+
+        std::unordered_map<char, int> count;
+
+        count.reserve(s.size());
+
+        for (char c : s) count[c]++;
+
+        for (char c : t) {
+            if (--count[c] < 0) return false;
+        }
+
+        return true;
+    }
+};
